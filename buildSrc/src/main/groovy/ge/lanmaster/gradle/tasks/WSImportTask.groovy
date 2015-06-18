52495e6efc87ce39fd5d0c17e23b61570156960e
Alexander Dolidze <alexander.dolidze@gmail.com>
2015-06-19 01:44:05 +0400

    Initialize variables during instance creation of WSImportTask. See example in wsimportproject/build.gradle Signed-off-by: Alexander Dolidze <alexander.dolidze@gmail.com>

---
+++ buildSrc/src/main/groovy/ge/lanmaster/gradle/tasks/WSImportTask.groovy
@@ -0,0 +1,34 @@
+package ge.lanmaster.gradle.tasks
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.TaskAction
+import org.gradle.api.tasks.OutputDirectory
+
+class WSImportTask extends DefaultTask {
+    
+    def List<String> wsdlUrls
+    
+    @OutputDirectory
+    File outDir
+            
+    @TaskAction
+    def wsimport() {
+        wsdlUrls.each() {
+            def temp = it
+            println "run wsimport for "+ temp
+            project.sourceSets.main.output.classesDir.mkdirs()
+            outDir.mkdirs()
+            def classpath = project.configurations.jaxws.asPath
+            def destDir = project.sourceSets.main.output.classesDir
+            project.ant {
+                taskdef(name:'wsimport',
+                    classname: 'com.sun.tools.ws.ant.WsImport',
+                    classpath: classpath)
+                wsimport(keep:true,
+                    destdir: destDir,
+                    sourcedestdir: outDir,
+                    wsdl: temp)
+            }
+        }
+    }
+}
---
+++ wscallproject/src/main/java/ge/lanmaster/wscallproject/Main.java
@@ -0,0 +1,13 @@
+package ge.lanmaster.wscallproject;
+
+import com.ebay.marketplace.search.v1.services.FindingService;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //just sample code to show that generated sources are reachable
+        FindingService service = new FindingService();
+        System.out.println("ItWorks = " + (service != null));
+    }
+
+}
